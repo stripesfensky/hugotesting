7a2aef885186ec9ba059de0ea27024feddf19ec3
Stripes <88601301+stripesfensky@users.noreply.github.com>
2025-09-23 22:22:39 -0400

    Update test.js

--- static/decap/test.js
+++ static/decap/test.js
@@ -1,4 +1,4 @@
-import PromptButtonWidget from './PromptButtonWidget'; // Import the new widget
+import PromptButtonWidget from './PromptButtonWidget.js'; // Import the new widget
 
 // Register the custom widget.
 CMS.registerWidget('prompt-button-widget', PromptButtonWidget);
